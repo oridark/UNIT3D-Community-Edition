d9285aa4b833975d6484618da5478664daf4ac9a
tianjiachao <tianjiachaonj@163.com>
2024-08-28 00:35:08 +0800

    修改安全配置中，允许访问192.168.0.235接口

--- config/secure-headers.php
+++ config/secure-headers.php
@@ -490,6 +490,7 @@ return [
                 'https://'.parse_url(env('APP_URL'), PHP_URL_HOST).':8443/socket.io/',
                 'wss://'.parse_url(env('APP_URL'), PHP_URL_HOST).':8443/socket.io/',
                 'https://api.themoviedb.org/',
+                'https://192.168.0.235/',
             ],
         ],
 
